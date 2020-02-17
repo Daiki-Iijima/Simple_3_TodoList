1942c17a9f0627ed9fae1b1a5e4a342873ea7b38
Daiki Iijima <daiki.applecreate@gmail.com>
2020-02-17 23:33:41 +0900

    事前に記述した配列をTableViewに表示できるようになった

--- Simple_3_TodoList/ViewController.swift
+++ Simple_3_TodoList/ViewController.swift
@@ -8,7 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    let TODO = ["A","B","C"]
+    
+    // セルの個数を指定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+    
+    //  セルの値を指定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //  セルを取得
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+    
+        //  セルに表示する値を設定
+        cell.textLabel!.text = TODO[indexPath.row]
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
